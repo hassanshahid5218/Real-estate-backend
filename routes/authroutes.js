ece002b6cb4a22544d71ed1131edc236606bc888
Muhammad hassan <hassanshahid5218@gmail.com>
2026-04-03 16:20:16 +0500

    restore auth

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -5,7 +5,6 @@ const router=exp.Router();
 router.post('/signup', signup)
 router.post('/signin', signin)
 router.post('/google', handlegoogle)
-router.get('/google', (req, res) => res.status(405).json({ success: false, message: 'Use POST /api/auth/google with Google user info (email,name,photo).'}))
 router.put('/update/:id', updateUser);
 router.get('/signout', handlesignout);
 
